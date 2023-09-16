bb7b849aea7616efc3e89bde0607bbf06dfae6f5
Simon Berger <simon@siku2.io>
2023-09-16 23:10:29 +0000

    gather diagnostics in parallel

--- custom_components/vzug/diagnostics.py
+++ custom_components/vzug/diagnostics.py
@@ -1,3 +1,4 @@
+import asyncio
 from collections.abc import Awaitable
 from typing import Any
 
@@ -28,23 +29,24 @@ async def gather_full_api_sample(shared: Shared) -> dict[str, Any]:
         except Exception as exc:
             data[key] = _serialize_exception(exc)
 
-    await do_one("mac_address", shared.client.get_mac_address())
-    await do_one("model_description", shared.client.get_model_description())
-    await do_one("device_status", shared.client.get_device_status())
-    await do_one("update_status", shared.client.get_update_status())
-    await do_one("check_for_updates", shared.client.check_for_updates())
-    await do_one("last_push_notifications", shared.client.get_last_push_notifications())
-    await do_one("list_categories", shared.client.list_categories())
-    await do_one("hh_fw_version", shared.client.get_hh_fw_version())
-    await do_one("ai_fw_version", shared.client.get_ai_fw_version())
-    await do_one("zh_mode", shared.client.get_zh_mode())
-    await do_one("eco_info", shared.client.get_eco_info())
-    await do_one("device_info", shared.client.get_device_info())
-    await do_one("program", shared.client.get_program())
-    await do_one("all_program_ids", shared.client.get_all_program_ids())
-
-    # config aggregate
-    await do_one("aggregate_config", shared.client.aggregate_config())
+    await asyncio.gather(
+        do_one("mac_address", shared.client.get_mac_address()),
+        do_one("model_description", shared.client.get_model_description()),
+        do_one("device_status", shared.client.get_device_status()),
+        do_one("update_status", shared.client.get_update_status()),
+        do_one("check_for_updates", shared.client.check_for_updates()),
+        do_one("last_push_notifications", shared.client.get_last_push_notifications()),
+        do_one("list_categories", shared.client.list_categories()),
+        do_one("hh_fw_version", shared.client.get_hh_fw_version()),
+        do_one("ai_fw_version", shared.client.get_ai_fw_version()),
+        do_one("zh_mode", shared.client.get_zh_mode()),
+        do_one("eco_info", shared.client.get_eco_info()),
+        do_one("device_info", shared.client.get_device_info()),
+        do_one("program", shared.client.get_program()),
+        do_one("all_program_ids", shared.client.get_all_program_ids()),
+        # config aggregate
+        do_one("aggregate_config", shared.client.aggregate_config()),
+    )
 
     return data
 
--- docs/api_versions.md
+++ docs/api_versions.md
@@ -4,3 +4,4 @@
 
 - Doesn't have the `alterable` key in commands. All commands are alterable by default.
 - Returns `null` instead of an empty list for `getCategories` when there are no categories.
+- `getUpdateStatus` isn't supported.
