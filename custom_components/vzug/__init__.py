dd6415e69d31aa54ba5df2af292c8731f748bbfa
Simon Berger <simon@siku2.io>
2025-03-01 13:34:04 +0000

    Clean up and fix formatting

--- custom_components/vzug/__init__.py
+++ custom_components/vzug/__init__.py
@@ -3,7 +3,8 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry as dr, entity_registry as er
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers import entity_registry as er
 from yarl import URL
 
 from . import api
@@ -52,59 +53,65 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return unload_ok
 
 
-async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    _LOGGER.debug("migrating from version %s.%s", config_entry.version, config_entry.minor_version)
+async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    version_tuple = (entry.version, entry.minor_version)
+    _LOGGER.debug("migrating from version %s.%s", entry.version, entry.minor_version)
 
-    if config_entry.version == 2 and config_entry.minor_version == 2:
+    if version_tuple > (2, 2):
+        # downgrade is not supported
         return False
 
-    new_data = config_entry.data.copy()
+    new_data = entry.data.copy()
 
-    if config_entry.version == 1:
-        # migrate base_url
-        base_url = URL(config_entry.data["host"])
+    if version_tuple < (2, 0):
+        # version 2 switched from using 'host' to 'base_url' in the config to allow for http / https differentiation
+        base_url = URL(new_data.pop("host"))
         if not base_url.is_absolute():
             base_url = URL(f"http://{base_url}")
         new_data[CONF_BASE_URL] = str(base_url)
 
-    if config_entry.minor_version == 1:
-        # migrate old entity unique id
+    if version_tuple < (2, 2):
+        # version 2.2 switches from using 'device_uuid' or 'device_serial' to 'mac_addr' in the unique_id
         entity_reg = er.async_get(hass)
-        entities: list[er.RegistryEntry] = er.async_entries_for_config_entry(
-            entity_reg, config_entry.entry_id
-        )
+        entities = er.async_entries_for_config_entry(entity_reg, entry.entry_id)
 
         # setup coordinator to get required data for unique_id
         try:
             credentials = api.Credentials(
-                username=config_entry.data[CONF_USERNAME], password=config_entry.data[CONF_PASSWORD]
+                username=new_data[CONF_USERNAME],
+                password=new_data[CONF_PASSWORD],
             )
         except KeyError:
             credentials = None
         base_url = URL(new_data[CONF_BASE_URL])
         shared = Shared(hass, base_url, credentials)
         await shared.async_config_entry_first_refresh()
-        old_prefix = shared.state_coord.data.device.get("deviceUuid", "") or shared.state_coord.data.device.get("Serial", "")
+
+        old_prefix = shared.state_coord.data.device.get(
+            "deviceUuid", ""
+        ) or shared.state_coord.data.device.get("Serial", "")
         mac_addr = dr.format_mac(shared.meta.mac_address)
 
         for entity in entities:
             # migrate unique_id_prefix from 'device_uuid' or 'device_serial' to 'mac_addr'
-
             if old_prefix not in entity.unique_id:
                 continue
 
             new_uid = entity.unique_id.replace(old_prefix, mac_addr)
-            _LOGGER.debug(
-                "migrate unique id '%s' to '%s'", entity.unique_id, new_uid
-            )
-            entity_reg.async_update_entity(
-                entity.entity_id, new_unique_id=new_uid
-            )
+            _LOGGER.debug("migrate unique id '%s' to '%s'", entity.unique_id, new_uid)
+            entity_reg.async_update_entity(entity.entity_id, new_unique_id=new_uid)
 
-        hass.config_entries.async_update_entry(
-            config_entry, data={"base_url": str(base_url)}, version = 2, minor_version = 2
-        )
+    hass.config_entries.async_update_entry(
+        entry,
+        data=new_data,
+        version=2,
+        minor_version=2,
+    )
 
-    _LOGGER.debug("migration to version %s.%s successful", config_entry.version, config_entry.minor_version)
+    _LOGGER.debug(
+        "migration to version %s.%s successful",
+        entry.version,
+        entry.minor_version,
+    )
 
     return True
